7f975781c46eb6393c6efb25580965175d1e0abb
Chris Scott <chris@chrisdjscott.co.uk>
2025-06-05 12:17:38 +1200

    update example

--- examples/afr-progs-sym.jl
+++ examples/afr-progs-sym.jl
@@ -24,6 +24,10 @@ using Distributions
 
 cse_prob = SymmetricAfrprogsCSEProblem()
 
+# You can view the default options by running:
+
+Base.doc(SymmetricAfrprogsCSEProblem)
+
 # ## Compute the CSE
 #
 # Now compute the CSE for the problem we created

9061b34266ed5cd0d55227a93c7ee16547197216
Chris Scott <chris@chrisdjscott.co.uk>
2025-07-07 11:26:33 +1200

    working on tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using ConstrainedStrategicEquilibrium
+using DataFrames
 using Distributions
 using Test
 
 @testset verbose = true "ConstrainedStrategicEquilibrium tests" begin
     @testset verbose = true "Symmetric CSE tests" begin
         include("test_symmetric.jl")
+    end
+
+    @testset verbose = true "Asymmetric CSE tests" begin
+        include("test_afrprogs_asymmetric.jl")
     end
 end
--- test/test_symmetric.jl
+++ test/test_symmetric.jl
@@ -1,20 +1,41 @@
-@testset verbose = true "Test symmetric compute with 2 n values" begin
-    cse_prob = SymmetricAfrprogsCSEProblem(inin=2, maxn=4)
+@testset verbose = true "Test afprogs symmetric reference run for n=2..4" begin
+    cse_prob = SymmetricAfrprogsCSEProblem(inin=2, maxn=4, np=4)
     validate_cse_problem(cse_prob)
     solutions = compute_cse(cse_prob)
     @test length(solutions) == 2
+    @test solutions[1].success
+    @test isnan(solutions[1].c_1)
+    @test solutions[1].c_2 < 1e-10
+    @test solutions[2].mse < 1e-3
+    @test solutions[2].success
+    @test !isnan(solutions[2].c_1)
+    @test solutions[2].c_2 < 1e-10
+    @test solutions[2].c_2 < solutions[1].c_2
+    @test solutions[2].mse < 1e-4
 
-    sol = solutions[1]
-    @test sol.success
-    @test isnan(sol.c_1)
-    @test isapprox(sol.c_2, 3.715063021296306e-14)
-    @test isapprox(sol.mse, 0.00020157222130046955)
+    # load the reference data
+    refdata_path = joinpath(@__DIR__, "data", "afrprogs-sym-cse-reference.txt")
+    lines = readlines(refdata_path)
+    data = [parse.(Float64, split(replace(line, 'D' => 'E'))) for line in lines]
+    array_data = reduce(vcat, [reshape(row, 1, :) for row in data])
+    cse_ref = DataFrame(array_data, ["x", "CSE(x)"])
 
-    sol = solutions[2]
-    @test sol.success
-    @test isapprox(sol.c_1, 0.0009946887825929547)
-    @test isapprox(sol.c_2, 3.87197277083241e-17)
-    @test isapprox(sol.mse, 5.4743805339204674e-5)
+    # compare generated CSE to reference data
+    cse = solutions[1].cse[:, ["x", "CSE(x)"]]
+    rtol = 1e-6
+    atol = 1e-2
+    @test size(cse) == size(cse_ref)
+    if size(cse) != size(cse_ref)
+        @error "CSE dataframes have different sizes."
+    end
+    for col in ("x", "CSE(x)")
+        is_close = isapprox.(cse[!, col], cse_ref[!, col]; atol=atol, rtol=rtol)
+        @test all(is_close)
+        if !all(is_close)
+            bad_rows = findall(!, is_close)
+            @error "CSE dataframe differs for column `$col` at rows: " bad_rows
+        end
+    end
 end
 
 @testset verbose = true "Test validate symmetric problem" begin

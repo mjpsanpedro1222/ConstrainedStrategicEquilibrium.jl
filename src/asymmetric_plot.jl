cf4258b335afe3b8ad8236e0b3dedfb76b858014
Chris Scott <chris@chrisdjscott.co.uk>
2025-07-08 11:02:24 +1200

    add extra info on the asymmetric plot

--- src/asymmetric_plot.jl
+++ src/asymmetric_plot.jl
@@ -4,6 +4,16 @@
     ylabel --> "Bids"
     label := "CSE 1"
 
+    c1_string = ""
+    if !isnan(sol.c_1[:bidder1])
+        c1_string = @sprintf("(%.2e, %.2e)", sol.c_1[:bidder1], sol.c_1[:bidder2])
+    end
+    ftr = @sprintf("%s; C_2=%.2e", c1_string, sol.c_2)
+    titlefontsize --> 10
+    dist2 = length(sol.problem.distributions) == 2 ? sol.problem.distributions[2] : sol.problem.distributions[3]
+    diststring = @sprintf("(1) %s; (2) %s", simplify_distribution_string(repr(sol.problem.distributions[1])), simplify_distribution_string(repr(dist2)))
+    title --> "CSE for n=$(sol.n), np=$(sol.problem.np), mc=$(sol.problem.mc), asymmetric case\n$(diststring)\n$(ftr)"
+
     @series begin
         label := "CSE 2"
         sol.cse."x", sol.cse."CSE(x) 2"

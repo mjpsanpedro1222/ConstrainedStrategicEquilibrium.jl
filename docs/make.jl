a355e8d0668d0f7616f1764fd8a9961e06616cd6
Chris Scott <chris@chrisdjscott.co.uk>
2025-07-31 13:02:41 +1200

    update compare example and add to docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ const EXAMPLES_RELATIVE_PATH = "generated"
 examples = Dict(
     "afr-progs-sym" => "Symmetric CSE - piecewise linear",
     "jae-poly-1" => "Symmetric CSE - polynomial",
+    "afr-progs-asym-sym-compare" => "Asymmetric - symmetric CSE test",
 )
 
 @info "Generating examples"
@@ -71,6 +72,7 @@ pages = [
 format = Documenter.HTML(
     prettyurls=get(ENV, "CI", nothing) == "true",
     assets=String["assets/citations.css"],
+    size_threshold=500_000,
 )
 
 # citations
--- examples/afr-progs-asym-sym-compare.jl
+++ examples/afr-progs-asym-sym-compare.jl
@@ -1,8 +1,9 @@
-using Base: run_package_callbacks
-# # Symmetric CSE from afr-progs using the asymmetric code
+# # Symmetric - asymmetric CSE test case
 #
 # This example is using the asymmetric afrprogs code with two players with the
-# same distribution (so symmetric).
+# same distribution (i.e. symmetric). The purpose is to test that the asymmetric
+# code base can reproduce the symmetric result and we can also test this further
+# by comparing the result to the BNE.
 #
 # ## Install dependencies
 #
@@ -21,104 +22,117 @@ using Plots
 using Distributions
 using NonlinearSolve
 
+# ## Define some parameters
+#
+# Here we set the initial and final values of n.
 
-function run_problem(cse_prob)
-    # Now validate the problem we just created (this will throw an error if there is a mistake):
-    validate_cse_problem(cse_prob)
+inin = 2
+maxn = 6
 
-    # ## Compute the CSE
-    solutions = compute_cse(cse_prob)
+# ## Solve for 2 players
+#
+# Start from n=2 like the original sym.f code with the following initial guess:
 
-    # Compute the BNE
-    bnex = Vector{Float64}(undef, 101)
-    bney = Vector{Float64}(undef, 101)
-    for m = 1:101
-        ti = (m - 1.0) / 100.0
-        if ti == 0
-            true_bne = 0.0
-        else
-            true_bne = compute_bne(ti, cse_prob.distributions[1], cse_prob.np)
-        end
-        bnex[m] = ti
-        bney[m] = true_bne
+xguess = [1.58285, 1.02813, -1.66483]
+
+# Create the problem:
+
+cse_prob2 = AsymmetricAfrprogsCSEProblem(
+    inin=inin,
+    maxn=maxn,
+    np=2,
+    mc=10000,
+    solver_kwargs=(show_trace=Val(false), maxiters=2000, abstol=1e-6, reltol=1e-6),
+    solver_initial_guess=xguess,
+    distributions=[Beta(3, 3), Beta(3, 3)],
+    knot_refinement_strategy=:even_spacing,
+)
+
+# Compute the solution to the CSE problem for 2 players:
+
+solutions2 = compute_cse(cse_prob2)
+
+# We also compute the BNE for 2 players for the same problem:
+
+bne2x = Vector{Float64}(undef, 101)
+bne2y = Vector{Float64}(undef, 101)
+for m = 1:101
+    ti = (m - 1.0) / 100.0
+    if ti == 0
+        true_bne = 0.0
+    else
+        true_bne = compute_bne(ti, cse_prob2.distributions[1], cse_prob2.np)
     end
-
-    return (solutions=solutions, bnex=bnex, bney=bney)
+    bne2x[m] = ti
+    bne2y[m] = true_bne
 end
 
 
-function main()
-    inin = 2
-    maxn = 6
+# ## Solve for 4 players
+#
+# Define the initial guess and create the problem:
+xguess = [-log(0.25), -log(0.25), log(0.25)]
+cse_prob4 = AsymmetricAfrprogsCSEProblem(
+    inin=inin,
+    maxn=maxn,
+    np=4,
+    mc=10000,
+    solver_kwargs=(show_trace=Val(false), maxiters=2000, abstol=1e-6, reltol=1e-6),
+    solver_initial_guess=xguess,
+    distributions=[Beta(3, 3), Beta(3, 3)],
+    knot_refinement_strategy=:even_spacing,
+)
 
-    # ## Create the CSE problem for 2 players
-    #
-    # Start from n=2 like the original sym.f code with the following initial guess that seems
-    # to work OK initially:
+# Compute the solution to the CSE problem for 4 players:
 
-#    xguess = [1.76034, 1.26109, -2.92289]
-    xguess = [1.58285, 1.02813, -1.66483]
-    cse_prob2 = AsymmetricAfrprogsCSEProblem(
-        inin=inin,
-        maxn=maxn,
-        np=2,
-        mc=10000,
-        solver_kwargs=(show_trace=Val(false), maxiters=2000, abstol=1e-6, reltol=1e-6),
-        solver_initial_guess=xguess,
-        distributions=[Beta(3, 3), Beta(3, 3)],
-        knot_refinement_strategy=:even_spacing,
-    )
+solutions4 = compute_cse(cse_prob4)
 
-    # solve for 2 players
-    result2 = run_problem(cse_prob2)
+# We also compute the BNE for 4 players for the same problem:
 
-    ## now 4 players
-    xguess = [-log(0.25), -log(0.25), log(0.25)]
-    cse_prob4 = AsymmetricAfrprogsCSEProblem(
-        inin=inin,
-        maxn=maxn,
-        np=4,
-        mc=10000,
-        solver_kwargs=(show_trace=Val(false), maxiters=2000, abstol=1e-6, reltol=1e-6),
-        solver_initial_guess=xguess,
-        distributions=[Beta(3, 3), Beta(3, 3)],
-        knot_refinement_strategy=:even_spacing,
-    )
-    result4 = run_problem(cse_prob4)
-
-    #
-    # plot the last solution of each
-    if length(result2.solutions) != length(result4.solutions)
-        throw("solutions are not the same length")
+bne4x = Vector{Float64}(undef, 101)
+bne4y = Vector{Float64}(undef, 101)
+for m = 1:101
+    ti = (m - 1.0) / 100.0
+    if ti == 0
+        true_bne = 0.0
+    else
+        true_bne = compute_bne(ti, cse_prob4.distributions[1], cse_prob4.np)
     end
-    if (!result2.solutions[end].success) || (!result4.solutions[end].success)
-        throw("final solution not successful")
-    end
-
-    cseplot(
-        result2.solutions[end],
-        cse_label=Dict(:bidder1 => "CSE 1 (np=2)", :bidder2 => "CSE 2 (np=2)"),
-        cse_colour=Dict(:bidder1 => :1, :bidder2 => :2),
-        knot_label=Dict(:bidder1 => "", :bidder2 => ""),
-        dpi=300,
-    )
-
-    cseplot!(
-        result4.solutions[end],
-        cse_label=Dict(:bidder1 => "CSE 1 (np=4)", :bidder2 => "CSE 2 (np=4)"),
-        cse_colour=Dict(:bidder1 => :3, :bidder2 => :4),
-        knot_label=Dict(:bidder1 => "", :bidder2 => ""),
-        title="Comparing CSE against BNE for num players 2 and 4\nasym code but all bidders are Beta(3,3)",
-        dpi=300,
-    )
-
-    plot!(result2.bnex, result2.bney, label="BNE (np=2)", dpi=300, seriescolor=:5)
-    plot!(result4.bnex, result4.bney, label="BNE (np=4)", dpi=300, seriescolor=:6)
-
-    savefig("afr-progs-asym-sym-compare.png")
+    bne4x[m] = ti
+    bne4y[m] = true_bne
 end
 
+# ## Results
+#
+# First some sanity checking that both problems were solved successfully.
 
-if abspath(PROGRAM_FILE) == @__FILE__
-    main()
+if length(solutions2) != length(solutions2)
+    throw("solutions are not the same length")
 end
+if (!solutions2[end].success) || (!solutions4[end].success)
+    throw("final solution not successful")
+end
+
+# Plot the two CSE solutions and BNE solutions on the same graph:
+
+cseplot(
+    solutions2[end],
+    cse_label=Dict(:bidder1 => "CSE 1 (np=2)", :bidder2 => "CSE 2 (np=2)"),
+    cse_colour=Dict(:bidder1 => :1, :bidder2 => :2),
+    knot_label=Dict(:bidder1 => "", :bidder2 => ""),
+    dpi=300,
+)
+cseplot!(
+    solutions4[end],
+    cse_label=Dict(:bidder1 => "CSE 1 (np=4)", :bidder2 => "CSE 2 (np=4)"),
+    cse_colour=Dict(:bidder1 => :3, :bidder2 => :4),
+    knot_label=Dict(:bidder1 => "", :bidder2 => ""),
+    title="Comparing CSE against BNE for num players 2 and 4\nasym code but all bidders are Beta(3,3)",
+    dpi=300,
+)
+plot!(bne2x, bne2y, label="BNE (np=2)", dpi=300, seriescolor=:5)
+plot!(bne4x, bne4y, label="BNE (np=4)", dpi=300, seriescolor=:6)
+
+# Finally, save the figure to file:
+
+savefig("afr-progs-asym-sym-compare.png")
